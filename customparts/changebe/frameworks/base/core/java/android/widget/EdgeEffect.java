c45f9cbf4bd647e3af2f69bc17645eff064ae872
leegarchat <leegarchat@gmail.com>
2026-01-08 19:44:57 +0300

    try to fix

--- customparts/changebe/frameworks/base/core/java/android/widget/EdgeEffect.java
+++ customparts/changebe/frameworks/base/core/java/android/widget/EdgeEffect.java
@@ -1,18 +1,19 @@
-
 public class EdgeEffect {
     public EdgeEffect(@NonNull Context context, @Nullable AttributeSet attrs) {
         // ... стандартный код ...
-        // --- [CustomNativeParts] End line ---
+        // --- [CustomNativeParts] INIT ---
         com.android.internal.customNativeExtractParts.EdgeEffectHooks.init(this, context);
     }
 
     public boolean isFinished() {
-        // --- [CustomNativeParts] On start line ---
+        // --- [CustomNativeParts] IS_FINISHED ---
+        // Возвращает true только если физика и визуальная часть пружины полностью завершены
         if (com.android.internal.customNativeExtractParts.EdgeEffectHooks.isFinished(this)) {
             return true; 
         }
         // ... стандартный код ...
     }
+
     public void finish() {
         // --- [CustomNativeParts] FINISH ---
         com.android.internal.customNativeExtractParts.EdgeEffectHooks.finish(this);
@@ -20,29 +21,33 @@ public class EdgeEffect {
     }
 
     public void onPull(float deltaDistance, float displacement) {
-        // --- [CustomNativeParts] On start line ---
-        if (com.android.internal.customNativeExtractParts.NativePartsManager.onEdgeEffectPull(this, deltaDistance, displacement, mWidth, mHeight)) {
+        // --- [CustomNativeParts] ON_PULL ---
+        // Перехватывает событие. Если возвращает true, стандартный код не выполняется.
+        if (com.android.internal.customNativeExtractParts.EdgeEffectHooks.onPull(this, deltaDistance, displacement, mWidth, mHeight)) {
             return;
         }
         // ... стандартный код ...
     }
 
     public void onRelease() {
-        // --- [CustomNativeParts] On start line ---
+        // --- [CustomNativeParts] ON_RELEASE ---
+        // Запускает анимацию возврата пружины
         com.android.internal.customNativeExtractParts.EdgeEffectHooks.onRelease(this);
         // ... стандартный код ...
     }
 
     public void onAbsorb(int velocity) {
-        // --- [CustomNativeParts] On start line ---
-        com.android.internal.customNativeExtractParts.NativePartsManager.onEdgeEffectAbsorb(this, velocity, mHeight);
+        // --- [CustomNativeParts] ON_ABSORB ---
+        // Запускает анимацию инерции (fling)
+        com.android.internal.customNativeExtractParts.EdgeEffectHooks.onAbsorb(this, velocity, mHeight);
         // ... стандартный код ...
     }
 
 
     public boolean draw(Canvas canvas) {
-        // --- [CustomNativeParts] On start line ---
-        if (com.android.internal.customNativeExtractParts.NativePartsManager.onEdgeEffectDraw(this, canvas, mWidth, mHeight)) {
+        // --- [CustomNativeParts] DRAW ---
+        // Если возвращает true, значит пружина рисуется и мы пропускаем стандартный draw
+        if (com.android.internal.customNativeExtractParts.EdgeEffectHooks.draw(this, canvas, mWidth, mHeight)) {
             return true;
         }
         // ... стандартный код ..
