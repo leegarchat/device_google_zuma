d447e39d1303d603ac2b6e2bf4f1eef58af347b4
leegarchat <leegarchat@gmail.com>
2026-01-17 23:41:54 +0300

    Remove PixelExtraPart and migrate standalone repo

--- customparts/test/src/org/pixel/customparts/AppConfig.kt
+++
@@ -1,7 +0,0 @@
-package org.pixel.customparts
-
-object AppConfig {
-    const val ENABLE_THERMALS = true
-    const val IS_XPOSED = false
-    const val NEEDS_ROOT_ACCESS = true
-}
--- customparts/xposed/src/org/pixel/customparts/AppConfig.kt
+++
@@ -1,7 +0,0 @@
-package org.pixel.customparts
-
-object AppConfig {
-    const val ENABLE_THERMALS = false
-    const val IS_XPOSED = true
-    const val NEEDS_ROOT_ACCESS = true
-}
